54965997c74968e5150eb1241f74085caee18186
schindlerwork <schindler@xtendly.com>
2025-03-28 00:26:06 +0800

    feat: Tablet and Desktop View, Finished v.1

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,28 @@
 const cardButton = document.querySelector('.card__btn');
+const cardButtonDark = document.querySelector('.card__btn--dark');
+const cardButtonTablet = document.querySelector('.card__btn--md');
 const profileContainer = document.querySelector('.card__profile-container');
 const shareContainer = document.querySelector('.card__share');
 const profile = document.querySelector('.card__profile');
-const cardTextContainer = document.querySelector('.card__text');
-const shareIcon = document.querySelector('.card__share-icon');
+const shareToolTip = document.querySelector('.card__tooltip');
 
 cardButton.addEventListener("click", () => {
     shareContainer.classList.toggle("hidden");
     profile.classList.toggle("hidden")
-    if (shareContainer.checkVisibility()) {
-        profileContainer.style.padding = "1.375rem 1.875rem";
-        profileContainer.style.backgroundColor = "#48556a";
-        cardTextContainer.style.marginBottom = "1.875rem";
-    } else {
-        profileContainer.style.padding = "0 1.875rem 1.125rem";
-        profileContainer.style.backgroundColor = "#FFFFFF";
-        cardTextContainer.style.marginBottom = "3rem";
-    }
+    profileContainer.style.padding = "1.375rem 1.875rem";
+    profileContainer.style.backgroundColor = "#48556a";
+    profileContainer.style.marginTop = "1.875rem";
+});
+
+cardButtonDark.addEventListener("click", () => {
+    shareContainer.classList.toggle("hidden");
+    profile.classList.toggle("hidden");
+    profileContainer.style.marginTop = "3rem";
+    profileContainer.style.padding = "0";
+    profileContainer.style.backgroundColor = "#FFFFFF";
 })
+
+cardButtonTablet.addEventListener("click", () => {
+    shareToolTip.classList.toggle('visible')
+})
+
