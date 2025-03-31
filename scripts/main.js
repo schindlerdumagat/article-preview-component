b44dc2f7201cb16685550f1391a18b530c26e42f
schindlerwork <schindler@xtendly.com>
2025-04-01 02:27:41 +0800

    Refactor Everything

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,12 @@
 const cardButton = document.querySelector('.card__btn');
-const cardButtonDark = document.querySelector('.card__btn--dark');
-const cardButtonTablet = document.querySelector('.card__btn--md');
-const profileContainer = document.querySelector('.card__profile-container');
-const shareContainer = document.querySelector('.card__share');
-const profile = document.querySelector('.card__profile');
-const shareToolTip = document.querySelector('.card__tooltip');
+const shareDialog = document.querySelector('.card__share-dialog');
 
 cardButton.addEventListener("click", () => {
-    shareContainer.classList.toggle("hidden");
-    profile.classList.toggle("hidden")
-    profileContainer.style.padding = "1.375rem 1.875rem";
-    profileContainer.style.backgroundColor = "#48556a";
-    profileContainer.style.marginTop = "1.875rem";
-});
-
-cardButtonDark.addEventListener("click", () => {
-    shareContainer.classList.toggle("hidden");
-    profile.classList.toggle("hidden");
-    profileContainer.style.marginTop = "3rem";
-    profileContainer.style.padding = "0";
-    profileContainer.style.backgroundColor = "#FFFFFF";
+    if (!shareDialog.open) {
+        shareDialog.show();
+        cardButton.classList.add('card__btn--active');
+    } else {
+        shareDialog.close();
+        cardButton.classList.remove('card__btn--active');
+    }
 })
-
-cardButtonTablet.addEventListener("click", () => {
-    shareToolTip.classList.toggle('visible')
-})
-
