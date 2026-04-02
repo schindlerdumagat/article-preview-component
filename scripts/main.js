851bd921f9598810956fdc6a29f7544b674d5f85
schindlerwork <schindler@xtendly.com>
2026-04-02 22:52:21 +0800

    Version 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-const shareButton = document.querySelector(".card__btn");
+const shareButton = document.querySelector(".profile__btn");
 const dialog = document.querySelector(".share");
 
 shareButton.addEventListener("click", () => {
@@ -9,4 +9,16 @@ shareButton.addEventListener("click", () => {
         dialog.show();
     }
 
-})
+    updateButtonColor(dialog.open);
+
+});
+
+function updateButtonColor(isOpen) {
+
+    if(isOpen) {
+        shareButton.classList.add("profile__btn--open");
+    } else {
+        shareButton.classList.remove("profile__btn--open");
+    }
+
+}
