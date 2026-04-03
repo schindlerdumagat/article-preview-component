fee54634d6e663e645500a22664ed11529b7ba63
schindlerwork <schindler@xtendly.com>
2026-04-03 20:48:57 +0800

    Polished Version

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,41 @@
 const shareButton = document.querySelector(".profile__btn");
-const dialog = document.querySelector(".share");
+const shareOptions = document.querySelector(".share");
 
+// Opens and closes the popover UI when clicking the button
 shareButton.addEventListener("click", () => {
 
-    if (dialog.open) {
-        dialog.close();
-    } else {
-        dialog.show();
-    }
+    const isOpen = shareOptions.hasAttribute("data-open");
 
-    updateButtonColor(dialog.open);
+    shareOptions.toggleAttribute("data-open");
+    shareButton.classList.toggle("profile__btn--open");
+    
+    shareButton.setAttribute("aria-label", isOpen ? "Open share options" : "Close share options")
+    shareButton.setAttribute("aria-expanded", String(!isOpen));
+});
+
+// Closes the popover UI when clicking other things aside from the button and popover UI
+document.addEventListener("click", (e) => {
+
+    const isClickInside = shareButton.contains(e.target) || shareOptions.contains(e.target);
+
+    if (!isClickInside) {
+        closeShareOptions()
+    }
 
 });
 
-function updateButtonColor(isOpen) {
+// Closes the popover UI when pressing the escape key
+document.addEventListener("keydown", (e) => {
 
-    if(isOpen) {
-        shareButton.classList.add("profile__btn--open");
-    } else {
-        shareButton.classList.remove("profile__btn--open");
+    if (e.key === "Escape") {
+        closeShareOptions()
     }
 
+});
+
+function closeShareOptions() {
+    shareOptions.removeAttribute("data-open");
+    shareButton.classList.remove("profile__btn--open");
+    shareButton.setAttribute("aria-expanded", "false");
+    shareButton.setAttribute("aria-label", "Open share options");
 }
